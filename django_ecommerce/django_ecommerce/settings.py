596aaa6a020fc867e9423a1ad6934ad5060ad50a
pjlindsay <pjlindsay@gmail.com>
2015-02-18 23:02:43 -0800

    database upgraded to postgres and djangousr has correct CREATEDB privs for creating test db

--- django_ecommerce/django_ecommerce/settings.py
+++ django_ecommerce/django_ecommerce/settings.py
@@ -8,6 +8,7 @@ PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__))
 SITE_ROOT = os.path.dirname(PROJECT_ROOT)
 STRIPE_SECRET = 'sk_test_4QBquf6d5EzsnJC1fTI2GBGm'
 STRIPE_PUBLISHABLE = 'pk_test_4QBqqGvCk9gaNn3pl1cwxcAS'
+
 TEST_RUNNER = 'django.test.runner.DiscoverRunner'
 
 ADMINS = (
@@ -18,8 +19,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'test.db'
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_db',
+        'USER': 'djangousr',
+        'PASSWORD': 'geck0p!pg',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
