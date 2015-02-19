d79ab7207029f02a39dee567067ebc0bc59b39ea
pjlindsay <pjlindsay@gmail.com>
2015-02-18 20:45:17 -0800

    commit for page 119

--- django_ecommerce/contact/forms.py
+++ django_ecommerce/contact/forms.py
@@ -5,7 +5,7 @@ from django import forms
 
 class ContactView(ModelForm):
     message = forms.CharField(widget=forms.Textarea)
-
+    
     class Meta:
-	fields = ['name', 'email', 'topic', 'message']
+        fields = ['name', 'email', 'topic', 'message']
         model = ContactForm
